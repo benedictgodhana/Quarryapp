94cd1b61ca64bef874974939daf57165fbe9b352
benedictgodhana <benedictgodhana7@gmail.com>
2023-10-29 22:25:11 +0300

    fist commit

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -91,64 +91,40 @@
         </v-btn>
         
       </template>
-      <v-card>
-        <v-card-title color="info">
-          <span class="text-h5"> ADD - UPDATE ACCOUNT CLIENT</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container style="width:500px;">
-            <v-row>
-             
-              <v-col cols="12">
-                <v-text-field
-                  label="Product Name*"
-                  required
-                  variant="outlined"
-                ></v-text-field>
-              </v-col>
-             
-              <v-col
-                cols="12"
-              >
-                <v-select
-                  :items="['1', '2', '3', '4']"
-                  label="Grade/ID*"
-                  required
-                  variant="outlined"
-                ></v-select>
-              </v-col>
-              <v-col
-                cols="12"
-              >
-                <v-autocomplete
-                  :items="['Active', 'Inactive']"
-                  label="Status"
-                  variant="outlined"
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="blue-darken-1"
-            variant="text"
-            @click="dialog = false"
-          >
-            Close
-          </v-btn>
-          <v-btn
-            color="blue-darken-1"
-            variant="text"
-            @click="dialog = false"
-          >
-            Save
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+      <v-form @submit.prevent="addProduct">
+    <v-card style="border-radius:10px">
+      <v-card-title color="info" class="bg-info" style="margin-top:;text-align: center;">
+        <span class="text-h5"> ADD - UPDATE ACCOUNT CLIENT</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container style="width:500px; border-radius: ;">
+          <v-row>
+            <v-col cols="12">
+              <v-text-field v-model="newProduct.product_name" label="Product Name*" required variant="outlined"></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-select v-model="newProduct.grade" :items="['1', '2', '3', '4', '5', '6']" label="Grade/ID*" required variant="outlined"></v-select>
+            </v-col>
+            <v-col cols="12">
+              <v-autocomplete v-model="newProduct.status" :items="statusOptions" label="Status" variant="outlined"></v-autocomplete>
+            </v-col>
+          </v-row>
+        </v-container>
+        <small>*indicates required field</small>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="red darken-2" elevation="6" variant="outlined" @click="dialog = false">
+    <v-icon left>mdi-close</v-icon> Close
+  </v-btn>
+  <v-btn color="blue darken-2" elevation="6" variant="outlined" @click="addProduct">
+    <v-icon left>mdi-check</v-icon> Save
+  </v-btn>             
+       </v-card-actions>
+    </v-card>
+  </v-form>
+
+       </v-dialog>
   </v-row>
                 </div>
                 <v-card-text style="margin-top:-48px">
@@ -167,15 +143,20 @@
       </v-form>
     <v-table
     fixed-header
-    height="300px"
+    height="550px"
     class="custom-table"
+    :items="products"
     >
     <thead>
     
     <tr>
-        <th class="text-left">
+      <th class="text-left">
             #
         </th>
+      <th class="text-left">
+            ID
+        </th>
+        
       <th class="text-left">
         Product Name
       </th>
@@ -191,33 +172,18 @@
     </tr>
   </thead>
   <tbody>
-    <tr v-for="product in products" :key="product.id">
+    <tr v-for="(product) in products" :key="product.id">
     <td> <v-checkbox v-model="product.selected"></v-checkbox></td>
-      <td>{{ product.name }}</td>
-      <td>{{ product.grade }}</td>
-      <td>{{ product.status }}</td>
+    <td>{{ product.id}}</td>
+      <td>{{ product.product_name}}</td>
+      <td>{{ product.grade}}</td>
+      <td>{{ product.status}}</td>
       <td>
         <!-- Add actions here, e.g., Edit and Delete buttons -->
         <v-btn class="mr-4" color="success" @click="editProductDialog = true">
       <v-icon left>mdi-pencil</v-icon> Edit
     </v-btn>        
-    <v-dialog v-model="editProductDialog" max-width="500">
-      <v-card>
-        <v-card-title>Edit Product</v-card-title>
-        <v-card-text>
-          <!-- Edit product form -->
-          <v-form @submit.prevent="saveProductChanges">
-            <v-text-field v-model="editedProduct.name" label="Product Name" variant="outlined"></v-text-field>
-            <v-text-field v-model="editedProduct.grade" label="Grade/ID" variant="outlined"></v-text-field>
-            <v-select v-model="editedProduct.status" :items="statusOptions" label="Status" variant="outlined"></v-select>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="blue darken-1" text @click="editProductDialog = false">Cancel</v-btn>
-          <v-btn color="success" @click="saveProductChanges">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>    
+    
     <v-btn color="red" @click="deleteProduct(product)">
   <v-icon left>mdi-delete</v-icon> Delete
 </v-btn>
@@ -225,14 +191,57 @@
       </td>
     </tr>
   </tbody>
+    <template v-slot:bottom>
+      <div class="text-center pt-2">
+        <v-pagination
+          v-model="page"
+          :length="pageCount"
+        ></v-pagination>
+        
+      </div>
+    </template>
 </v-table>
 </v-card-text>
 
               </v-card>
             </v-container>
           </v-main>
+          <v-dialog v-model="editProductDialog" max-width="500">
+      <v-card style="border-radius: 10px;">
+        <v-card-title color="info" class="bg-info" style="margin-top:;text-align: center;">Edit Product</v-card-title>
+        <v-card-text>
+          <!-- Edit product form -->
+          <v-form @submit.prevent="saveProductChanges">
+            <v-text-field v-model="editedProduct.name" label="Product Name" variant="outlined"></v-text-field>
+            <v-select v-model="newProduct.grade" :items="['1', '2', '3', '4', '5', '6']" label="Grade/ID*" required variant="outlined"></v-select>
+            <v-select v-model="editedProduct.status" :items="statusOptions" label="Status" variant="outlined"></v-select>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="blue darken-2" elevation="6" variant="outlined" text @click="editProductDialog = false">Cancel</v-btn>
+          <v-btn color="success darken-2" elevation="6" variant="outlined" @click="saveProductChanges">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>    
+          <!-- Delete Form -->
 
-          
+          <v-dialog v-model="deleteProductDialog" max-width="500">
+  <v-card style="border-radius:10px">
+    <v-card-title>Confirm Delete</v-card-title>
+    <v-card-text>
+      Are you sure you want to delete {{ productToDelete.product_name }}?
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="red" text @click="deleteProductDialog = false">
+        <v-icon left>mdi-close</v-icon> Cancel
+      </v-btn>
+      <v-btn color="blue" text @click="confirmDeleteProduct">
+        <v-icon left>mdi-check</v-icon> Confirm
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</v-dialog>
+
 
         </v-layout>
       </v-card>
@@ -240,39 +249,133 @@
   </template>
   
   <script>
-  import { ref } from 'vue';
-  
-  export default {
-    data() {
-      return {
-        drawer: true,
-        rail: true,
-        isDrawerOpen: false, // Add this line to define isDrawerOpen
-        dialog: false,
-        editProductDialog: false,
-        statusOptions: ['Active', 'Inactive'], // Sample status options
-        editedProduct: {
-        name: '',  // Initialize with empty values
+import axiosInstance from '../service/api'; // Import the Axios instance you provided
+
+export default {
+  data() {
+    return {
+      drawer: true,
+      rail: true,
+      products:[],
+      deleteProductDialog: false, // Initialize as false
+      productToDelete: null, // Initialize as null
+      isDrawerOpen: false,
+      dialog: false,
+      editProductDialog: false,
+      page: 1,
+      itemsPerPage: 5,
+      statusOptions: ['Active', 'Inactive'],
+      editedProduct: {
+        name: '',
         grade: '',
         status: '',
+
       },
-        products: [
-        { id: 1, name: 'Product 1', grade: 'A', status: 'Active' },
-        { id: 2, name: 'Product 2', grade: 'B', status: 'Inactive' },
-        { id: 3, name: 'Product 3', grade: 'C', status: 'Active' },
-        
-        // Add more product objects as needed
-      ],
-      };
-    },
-    methods: {
-      toggleDrawer() {
-        this.isDrawerOpen = !this.isDrawerOpen;
+      newProduct: {
+        name: '',
+        grade: '',
+        status: 'Active', // Default status
+      },
+      
+    };
+    
+  },
+    
+ created(){
+  this.getProducts();
+ },
+ computed: {
+      pageCount () {
+        return Math.ceil(this.products.length / this.itemsPerPage)
       },
     },
-  };
-  </script>
   
+  methods: {
+    toggleDrawer() {
+      this.isDrawerOpen = !this.isDrawerOpen;
+    },
+
+    deleteProduct(product) {
+  this.productToDelete = product; // Store the product to be deleted
+  this.deleteProductDialog = true; // Open the delete confirmation modal
+},
+
+   // Update the page and fetch data for the specified page
+   getProducts(pageNumber, perPage) {
+  this.page = pageNumber; // Update the current page
+  axiosInstance.get('getProducts', {
+    params: {
+      page: pageNumber, // Send the requested page
+      per_page: perPage, // Send the number of items per page
+    }
+  })
+  .then((response) => {
+    console.log("Received products for page " + pageNumber);
+    this.products = response.data.data; // Update your product data
+    this.totalProducts = response.data.total; // Update the total product count
+  })
+  .catch((error) => {
+    console.error('Failed to fetch products:', error);
+  });
+},
+
+prevPage() {
+  if (this.page > 1) {
+    this.page--;
+    console.log("Previous page: " + this.page);
+    this.getProducts(this.page, this.itemsPerPage);
+  }
+},
+
+nextPage() {
+  if (this.page < this.pageCount) {
+    this.page++;
+    console.log("Next page: " + this.page);
+    this.getProducts(this.page, this.itemsPerPage);
+  }
+},
+
+
+addProduct() {
+      // Send a POST request to add a new product
+      axiosInstance.post('addProduct', this.newProduct)
+        .then((response) => {
+          // Handle the response, e.g., show a success message and update the product list
+          this.dialog = false; // Close the dialog          
+          this.getProducts(); // Fetch the updated product list
+        })
+        .catch((error) => {
+          // Handle errors, e.g., show an error message
+          console.error('Error adding product:', error);
+        });
+    },
+    
+  },
+
+  confirmDeleteProduct() {
+    if (this.productToDelete) {
+      const productId = this.productToDelete.id; // Assuming your product has an 'id' property
+
+      // Send an HTTP request to delete the product by ID
+      axiosInstance.delete(`deleteProduct/${productId}`)
+        .then((response) => {
+          // Handle the successful deletion, e.g., show a success message
+          this.deleteProductDialog = false; // Close the delete confirmation dialog
+          this.getProducts(); // Fetch the updated product list
+        })
+        .catch((error) => {
+          // Handle errors, e.g., show an error message
+          console.error('Error deleting product:', error);
+        });
+    } else {
+      console.error('No product to delete.');
+    }
+  },
+  // Use the onMounted hook to fetch products when the component is mounted
+  
+};
+</script>
+
   <style scoped>
   .fixed-content {
     height: calc(100vh - 4px); /* 64px is the height of the app bar */
